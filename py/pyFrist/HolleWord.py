dc4e3957e1231d68df1a8e778548dc9439105465
xsd <2387145195@qq.com>
2020-05-20 17:01:10 +0800

    首次提交

---
+++ py/pyFrist/HolleWord.py
@@ -0,0 +1,19 @@
+print("holleWord")
+print("holleWord!!!")
+age=24
+money=18.8
+name='xiaoshengda'
+USER="xsd";
+print(" 姓名：%s,年龄：%s" %(name,age))
+
+USER="wh"
+print(USER)
+
+desc='''
+肖圣达
+520
+王慧
+'''
+print(desc)
+msg="{}今年 {}".format(name, age)
+print(msg)
---
+++ py/pyFrist/If.py
@@ -0,0 +1,21 @@
+NAME = "xiaoshengda"
+name = 'xiaoshengda'
+result = True if (NAME.__eq__(name))  else False
+print(result)
+password = ""
+if NAME.__eq__(name) or password :
+    print("您已经登录")
+    
+else:
+    print("您没有登录")
+nameList=('王慧','肖圣达','张三','李四','王慧')
+reNameList=set(nameList)
+print(reNameList)
+for i in nameList:
+    print(i)
+print("=========================================")
+for i in range(0):
+    print('当前：%s'%(i))
+else:print('执行完毕')
+
+
