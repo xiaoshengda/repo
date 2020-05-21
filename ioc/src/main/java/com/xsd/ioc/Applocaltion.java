fac0adef91a1ef2ab2f538cce330f57fdab9121d
xsd <2387145195@qq.com>
2020-05-21 10:15:00 +0800

    修改启动类

--- ioc/src/main/java/com/xsd/ioc/Applocaltion.java
+++ ioc/src/main/java/com/xsd/ioc/Applocaltion.java
@@ -3,7 +3,11 @@ package com.xsd.ioc;
 import com.xsd.ioc.core.XmlClassPathSpringIoc;
 import com.xsd.ioc.model.IocInfo;
 import com.xsd.ioc.service.IocService;
-
+/**
+ * 启动类
+ * @author wh
+ *
+ */
 public class Applocaltion
 {
 	public static void main(String[] args) throws Exception
